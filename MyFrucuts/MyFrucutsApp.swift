b05cad0521c05546021e9c84bbaf560834eeeddb
Mohan Chaudhari <mohan.chaudhari@globant.com>
2022-08-02 16:09:34 +0530

    Initial Commit

---
+++ MyFrucuts/MyFrucutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFrucutsApp.swift
+//  MyFrucuts
+//
+//  Created by Mohan Chaudhari on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFrucutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
